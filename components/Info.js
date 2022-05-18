047193a647a708262659d8ad614428231da3db4b
Stupidkid02 <jose.borges@unesp.br>
2022-05-17 23:17:26 -0300

    Commit inicial

---
+++ components/Info.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+export default function Info() {
+    return (
+    <div>
+    <img className = "img-info" src="../images/Jiboia-prateada-3.jpg" />
+    <h1>Laura Smith</h1>
+    <h4>Frontend Developer</h4>
+    <p>laurasmith.website</p>
+    </div>
+    )
+}
+
+
+
+
+
+
+
+
